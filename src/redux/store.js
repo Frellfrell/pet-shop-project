e16486cef632ccdd18c86ff8ac46af28f5024886
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-02 17:41:09 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "./reducers/categories.reducer";
+import productsReducer from "./reducers/productsReducer";
 
 export const store = configureStore({
   reducer: {
-    categories: categoriesReducer
+    categories: categoriesReducer,
+    products: productsReducer,
   }
 });
