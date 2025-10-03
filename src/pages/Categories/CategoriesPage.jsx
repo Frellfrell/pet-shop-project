a1a2665338b42ba1bbf0291834c6ae04db4ecff1
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-03 19:34:48 +0200

    Update CategoriesPage.jsx

--- src/pages/Categories/CategoriesPage.jsx
+++ src/pages/Categories/CategoriesPage.jsx
@@ -18,7 +18,7 @@ const CategoriesPage = () => {
   return (
     <div style={{ padding: '40px 0px' }}>
       <BreadCrumbs breadCrumbs={breadCrumbsData} />
-       <CategoriesSection hideAllCategories={true} />
+       <CategoriesSection hideAllCategories={true} limit={8} />
     </div>
   );
 };
