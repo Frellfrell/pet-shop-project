a284ca7fc9aee7e7b4aea9a7546f26c9d025a8ba
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-03 19:40:25 +0200

    Update MainPage.jsx

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -8,7 +8,7 @@ const MainPage = () => {
   return (
     <div>
       <PromoBanner />
-      <CategoriesSection limit={4} />
+      <CategoriesSection hideAllCategories={false} limit={4} />
       <DiscountOffer />
       <Sales />
     </div>
