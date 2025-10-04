8ef9abacd0ef0e93b305faa56dc23c622945f1b2
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-04 11:59:46 +0200

    Update CategoryProductsSection.jsx

--- src/components/CategoryProductsSection/CategoryProductsSection.jsx
+++ src/components/CategoryProductsSection/CategoryProductsSection.jsx
@@ -2,14 +2,29 @@
 import ProductCard from "../ProductCard/ProductCard";
 import FilterSet from "../Filter/FilterSet";
 import styles from "./CategoryProductsSection.module.css";
+import { BASE_URL } from "../../constants";
 
-const CategoryProductsSection = ({ products, isDiscountPage = false }) => {
+const CategoryProductsSection = ({ categoryId, isDiscountPage = false }) => {
+  const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  // Инициализация фильтрованных продуктов
-  useEffect(() => {
-    setFilteredProducts(products);
-  }, [products]);
+  
+ useEffect(() => {
+    if (!categoryId) return;
+
+    fetch(`${BASE_URL}/categories/${categoryId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const categoryProducts = data.products || data;
+        
+        setProducts(categoryProducts);
+        setFilteredProducts(categoryProducts);
+      })
+      .catch((error) => console.error("Ошибка при загрузке продуктов:", error));
+  }, [categoryId]);
+
+
+
   return (
     <section className={styles.productsSection}>
       {/* Фильтры */}
@@ -23,7 +38,7 @@ const CategoryProductsSection = ({ products, isDiscountPage = false }) => {
 
       {/* Сетка карточек продуктов */}
       <div className={styles.cardsContainer}>
-        {filteredProducts.length >= 8 ? (
+        {filteredProducts.length > 0 ? (
           filteredProducts.map((product) => (
             <ProductCard
               key={product.id}
