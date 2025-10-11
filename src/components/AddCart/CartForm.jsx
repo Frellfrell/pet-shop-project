41610e085896529e703216d157e05c5a8dc971c9
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-11 12:00:39 +0200

    Update CartForm.jsx

--- src/components/AddCart/CartForm.jsx
+++ src/components/AddCart/CartForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { spacing, button } from "../../constants/styles";
+//import { spacing, button } from "../../constants/styles";
+import { Box, TextField, Button, Typography } from "@mui/material";
+import PageTitle from "../PageTitle/PageTitle";
 
 const CartForm = ({ cartItems }) => {
   const { register, handleSubmit } = useForm();
@@ -18,12 +20,12 @@ const CartForm = ({ cartItems }) => {
       totalItems += 1
       console.log(totalItems)
 
-    })
+    });
 
     return { totalItems, totalPrice }
-  }
+  };
 
-  const { totalItems, totalPrice } = calculateTotals(cartItems)
+  const { totalItems, totalPrice } = calculateTotals(cartItems);
 
   const onSubmit = (data) => {
     console.log("Order submitted:", { data, cartItems });
@@ -31,15 +33,20 @@ const CartForm = ({ cartItems }) => {
   };
 
   return (
-    <div style={{ width: "484px", display: "flex", flexDirection: "column", gap: spacing.lg }}>
-      {/* Summary */}
-      <div style={{ display: "flex", justifyContent: "space-between", height: "122px" }}>
+     <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ display: "flex", flexDirection: "column", gap: 2 }} >
+      <PageTitle text="Order details" variant="small" />
+      <Typography >{totalItems} items</Typography>
+      <Typography >Total {totalPrice}$</Typography>
+    {/*<div style={{ width: "484px", display: "flex", flexDirection: "column", gap: spacing.lg }}>
+      {/* Summary */
+      /*<div style={{ display: "flex", justifyContent: "space-between", height: "122px" }}>
         <div>{+totalItems} items</div>
         <div>Total: {+totalPrice}$</div>
-      </div>
+      </div>*/}
+      
 
       {/* Form */}
-      <form onSubmit={handleSubmit(onSubmit)} style={{ display: "flex", flexDirection: "column", gap: spacing.md }}>
+     {/*} <form onSubmit={handleSubmit(onSubmit)} style={{ display: "flex", flexDirection: "column", gap: spacing.md }}>
         <input {...register("name", { required: true })} placeholder="Name" style={{ height: 58, padding: spacing.sm }} />
         <input {...register("phone", { required: true })} placeholder="Phone" style={{ height: 58, padding: spacing.sm }} />
         <input {...register("email", { required: true })} placeholder="Email" style={{ height: 58, padding: spacing.sm }} />
@@ -47,6 +54,15 @@ const CartForm = ({ cartItems }) => {
       </form>
     </div>
   );
-};
+};*/}
+ <TextField label="Name" variant="outlined" {...register("name")} />
+      <TextField label="Phone" variant="outlined" {...register("phone")} />
+      <TextField label="Email" variant="outlined" {...register("email")} />
+ <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+         Order
+      </Button>
+    </Box>
+)};
+
 
 export default CartForm;
