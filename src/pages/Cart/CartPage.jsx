46f4d7cb4003f352be6ad57efaec629b92e4739e
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-11 14:02:01 +0200

    Update CartPage.jsx

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -16,6 +16,7 @@ const CartPage = () => {
   useEffect(() => {
     setCartItems(getCartItems());
   }, [cartCounter]);
+  const isMobile = window.innerWidth < 1024;
 
   return (
     <div style={{ display: "flex", flexDirection: "column", padding: `${spacing.xl} ${spacing.xl}` }}>
@@ -23,7 +24,7 @@ const CartPage = () => {
       <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: spacing.lg }}>
        <PageTitle text="Shopping cart" />
         <div style={{ display: "flex", alignItems: "center", gap: spacing.sm }}>
-          <div style={{ width: "832px", height: "1px", backgroundColor: colors.grayDivider }} />
+          <div style={{ width: "700px", height: "1px", backgroundColor: colors.grayDivider }} />
           <Link to="/categories" style={{ color: colors.txtGrey, textDecoration: "none", fontWeight: "500", padding: spacing.sm, borderRadius: radii.small, border: `1px solid ${colors.txtGrey}` }}>
             All Categories
           </Link>
@@ -31,14 +32,35 @@ const CartPage = () => {
       </div>
 
       {/* Main content */}
-      <div style={{ display: "flex", gap: spacing.xl }}>
+      <div style={{ display: "flex", flexDirection: isMobile ? "column" : "row", gap: spacing.xl, width: "100%",
+          maxWidth: "1360px",
+          minHeight: "582px",
+          justifyContent: "space-between",
+           }}>
         {/* Left - Cart Items */}
-        <div style={{ display: "flex", flexDirection: "column", gap: "32px", flex: "1" }}>
-          {cartItems.length > 0 ? cartItems.map(item => <CartItem key={item.item.id} product={item.item} quantity={item.quantity} />) : <p>Your cart is empty</p>}
+        <div style={{ 
+          flex: isMobile ? "1" : "0 0 780px",
+          height: isMobile ? "auto" : "572px",
+          display: "flex", flexDirection: "column", gap: "32px", 
+           overflowY: isMobile ? "visible" : "auto",
+            paddingRight: "8px", 
+            scrollbarWidth: "thin", }}>
+          {cartItems.length > 0 ? cartItems.map(item => <CartItem key={item.item.id} product={item.item} quantity={item.quantity} 
+          style={{ width: "780px", height: "180px", flexShrink: 0, }} />) : <p>Your cart is empty</p>}
         </div>
 
         {/* Right - Cart Form */}
+        <div
+          style={{
+            flex: isMobile ? "1" : "0 0 548px",
+            height: isMobile ? "auto" : "582px",
+            padding: "32px",
+            backgroundColor: "rgba(241,243,243,1)",
+            borderRadius: radii.medium,
+          }}
+        >
         <CartForm cartItems={cartItems} />
+      </div>
       </div>
     </div>
   );
