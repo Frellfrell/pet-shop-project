d9d26d9e4f4be98b59debe735ad19a03d85b1b3a
Frellfrell <ella.gubareva.24@gmail.com>
2025-10-21 23:00:24 +0200

    Update CategoryPage.jsx

--- src/pages/Category/CategoryPage.jsx
+++ src/pages/Category/CategoryPage.jsx
@@ -6,7 +6,6 @@ import useScrollToTop from "../../components/hooks/useScrollToTop";
 import CategoryProductsSection from "../../components/CategoryProductsSection/CategoryProductsSection";
 import { fetchProductsByCategory } from "../../redux/actions/categories";
 
-// const selectCategories = (state) => state.categories.categories || [];
 
 const CategoryPage = () => {
   useScrollToTop();
@@ -18,31 +17,12 @@ const CategoryPage = () => {
     dispatch(fetchProductsByCategory(id))
   }, [dispatch, id]);
 
-
-  // const categories = useSelector(selectCategories);
   const category = useSelector((state) => state.categories.currentCategory);
   console.log(category)
   const categoryProducts = useSelector((state) => state.categories.categoryProducts);
   console.log(categoryProducts)
   const loading = useSelector((state) => state.categories.isLoading);
-  // const [category, setCategory] = useState(null);
-  // const [loading, setLoading] = useState(true);
-
-
-  // Загрузка категории
-  // useEffect(() => {
-  //   dispatch(fetchAllCategories());
-  // }, [dispatch]);
-  // Загрузка продуктов категории
-
-
-  // Находим текущую категорию по id
-  // useEffect(() => {
-  //   if (categories.length === 0) return;
-  //   const foundCategory = categories.find((c) => String(c.id) === String(id));
-  //   setCategory(foundCategory || null);
-  //   setLoading(false);
-  // }, [categories, id]);
+ 
 
   const breadCrumbs = [
     { name: "Main Page", path: "/" },
